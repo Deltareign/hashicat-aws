81dded6214c094e4509afb17e455a4456d03a387
jason cobian <jasoncobian10@gmail.com>
2022-07-18 13:13:21 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,16 +1,10 @@
 module "s3-bucket" {
   source  = "app.terraform.io/CDW-Jasocob/s3-bucket/aws"
   version = "2.8.0"
-}
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
   bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = "${var.prefix}"
-
-  versioning = {
+    versioning = {
     enabled = true
   }
 
